05906cc5dead35ca95a26cedfa19680d5a873158
Alvin <exhibition315@yahoo.com.tw>
2025-06-28 23:29:20 +0800

    1. Modify classToFunction

--- Javascript/Code/classToFunction.js
+++ Javascript/Code/classToFunction.js
@@ -4,36 +4,31 @@ class Example {
   constructor(name) {
     this.name;
   }
-  
+
   func() {
     console.log(this.name);
   }
 }
-
 
 'use strict'
 // ES5 Function
 function Example(name) {
   // 驗證 this 指向，因為 class 只能被 new 出來，不能直接呼叫
   if (!(this instanceof Example)) {
-    throw new TypeError('Class constructor Example cannot be invoked without new');
+    throw new TypeError(
+      "Class constructor Example cannot be invoked without new"
+    );
   }
 
   this.name = name;
 }
 
-Object.defineProperties(Example.prototype, 'func', {
-  value: function() {
-    // 不可通過 new 來調用
-    if (!(this instanceof Example)) {
-      throw new TypeError('Class constructor Example cannot be invoked without new');
-    }
-    
+// 定義方法到原型，且不可列舉
+Object.defineProperty(Example.prototype, "func", {
+  value: function () {
     console.log(this.name);
   },
-  enumerable: false,
-})
-
-Example.prototype.func = function() {
-  console.log(this.name);
-}
+  enumerable: false, // 跟 class 行為一樣，不可列舉
+  writable: true,
+  configurable: true,
+});
